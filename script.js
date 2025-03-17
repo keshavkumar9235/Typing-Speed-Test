b39a585edd93c423f55164e4db03a8d464a257a8
Keshav Kumar <114016606+keshavkumar9235@users.noreply.github.com>
2025-03-17 15:15:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+const typingText = document.querySelector('.typing-text p')
+const input = document.querySelector('.wrapper .text-fields')
+const time = document.querySelector('.time span b')
+const mistakes = document.querySelector('.mistake span')
+const wpm = document.querySelector('.wpm span')
+const cpm = document.querySelector('.cpm span')
+const btn = document.querySelector('button');
+
+//set value
+let timer;
+let maxTime= 60;
+let timeLeft = maxTime;
+let charIndex = 0;
+let mistake =0;
+let isTyping = false;
+
+
+function loadParagraph(){
+    const paragraph= [ " Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","The hefty opinion reveals itself as a sterile peer-to-peer to those who look. This could be, or perhaps the watch of a diamond becomes a bosom baboon. In recent years, some posit the unstuffed road to be less than altern. It's an undeniable fact, really; the livelong lettuce reveals itself as an unstuffed soda to those who look. In ancient times a bit is a balance's season. The popcorn of a morning becomes a moonless beauty.",
+    "If this was somewhat unclear, a friend is a fridge from the right perspective. An upset carriage is a stitch of the mind. To be more specific, a temper is a pair from the right perspective. Authors often misinterpret the liquid as a notchy baseball, when in actuality it feels more like an unbarbed angle. Though we assume the latter, the first vagrom report is, in its own way, a tower. We know that the octopus of a cd becomes an unrent dahlia.",
+    "A reptant discussion's rest comes with it the thought that the condemned syrup is a wish. The drake of a wallaby becomes a sonant harp. If this was somewhat unclear, spotty children show us how technicians can be jumps. Their honey was, in this moment, an intime direction. A ship is the lion of a hate. They were lost without the croupous jeep that composed their lily. In modern times a butcher of the birth is assumed to be a spiral bean.",
+    "Those cowbells are nothing more than elements. This could be, or perhaps before stockings, thoughts were only opinions. A coil of the exclamation is assumed to be a hurtless toy. A board is the cast of a religion. In ancient times the first stinko sailboat is, in its own way, an exchange. Few can name a tutti channel that isn't a footless operation. Extending this logic, an oatmeal is the rooster of a shake. Those step-sons are nothing more than matches.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required.","Subscribe to Drop X Out","Authors often misinterpret the flag as a wayless trigonometry, when in actuality it feels more like a bousy gold. Few can name a jasp oven that isn't a stutter grape. They were lost without the huffy religion that composed their booklet. Those waves are nothing more than pedestrians. Few can name a quartered semicolon that isn't a rounding scooter. Though we assume the latter, the literature would have us believe.",
+    "This could be, or perhaps few can name a pasteboard quiver that isn't a brittle alligator. A swordfish is a death's numeric. Authors often misinterpret the mist as a swelling asphalt, when in actuality it feels more like a crosswise closet. Some posit the tonal brother-in-law to be less than newborn. We know that the sizes could be said to resemble sleepwalk cycles. Before seasons, supplies were only fighters. Their stew was, in this moment.",
+    "The vision of an attempt becomes a lawny output. Dibbles are mis womens. The olden penalty reveals itself as a bustled field to those who look. Few can name a chalky force that isn't a primate literature. However, they were lost without the gamy screen that composed their beret. Nowhere is it disputed that a step-uncle is a factory from the right perspective. One cannot separate paints from dreary windows. What we don't know for sure is whether.",
+    "A tramp is a siamese from the right perspective. We know that a flitting monkey's jaw comes with it the thought that the submersed break is a pamphlet. Their cream was, in this moment, a seedy daffodil. The nest is a visitor. Far from the truth, they were lost without the released linen that composed their step-sister. A vibraphone can hardly be considered a pardine process without also being an archaeology. The bay of a hyacinth becomes."
+];
+
+const randomIndex = Math.floor(Math.random()*paragraph.length);
+typingText.innerHTML='';
+for(const char of paragraph[randomIndex]){
+console.log(char);
+typingText.innerHTML+= `<span>${char}</span>`;
+}
+typingText.querySelectorAll('span')[0].classList.add('active');
+document.addEventListener('keydown',()=>input.focus());
+typingText.addEventListener("click",()=>{
+    input.focus()})
+}
+
+// Handle user input
+function initTyping(){
+       const char= typingText.querySelectorAll('span');
+       const typedChar = input.value.charAt(charIndex);
+       if(charIndex < char.length && timeLeft >0){
+
+        if(!isTyping){
+            timer = setInterval(initTime,1000);
+            isTyping=true;
+        }
+
+        if(char[charIndex].innerText === typedChar){
+            char[charIndex].classList.add('correct');
+            console.log("correct");
+        }
+        else{
+            
+            char[charIndex].classList.add('incorrect');
+            console.log("incorrect");
+        }
+        charIndex++;
+        char[charIndex].classList.add('active');
+
+        mistakes.innerText = mistake;
+        cpm.innerText = charIndex- mistake;
+    }
+
+     else{
+     clearInterval(timer);
+     input.value='';
+      }
+  }
+
+function initTime(){
+    if(timeLeft>0){
+        timeLeft--;
+        time.innerText=timeLeft;
+        let wpmVal = Math.round(((charIndex - mistake)/5) /(maxTime - timeLeft)*60);
+        wpm.innerText = wpmVal;
+     }
+    else{
+        clearInterval(timer);
+    }
+ }
+
+function reset(){
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    time.innerText= timeLeft;
+    input.value='';
+    charIndex = 0;
+    mistake =0;
+    isTyping = false;
+    wpm.innerText=0;
+    cpm.innerText=0;
+    mistakes.innerText=0;
+}
+
+
+ 
+ input.addEventListener("input", initTyping);
+ btn.addEventListener("click",reset);
+ loadParagraph();
